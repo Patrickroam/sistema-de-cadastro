c47c28d079859f125542f9359fb56d9c0ede4930
Patrickroam <patrick.roan96@gmail.com>
2023-04-02 14:45:16 -0300

    consertando erro, gerando versão 3, e add pela 2° vez o script no html

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Rodando o sistema de gerenciamento de jogos)
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Consertando os erros passados");
+console.log("Versão 3");
+console.log("adicionando script no html pela segunda vez");
